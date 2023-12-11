c92948a64a9f6d1967875f44426dab9ea8edbe6f
alina.fedyniak <a.fedynyak@artjoker.team>
2023-12-11 23:54:45 +0200

    mobile cards, pagination

--- js/main.js
+++ js/main.js
@@ -135,4 +135,101 @@ document.addEventListener("DOMContentLoaded", function() {
     };
     initSortCards();
 
+    // -------burger-filter-cards----
+    const initBurgerFilterCards = () => {
+        const burgerIconFilters = document.querySelectorAll(".burger-iconFilter");
+        const burgerFilters = document.querySelectorAll(".burger-filter");
+        const closeButtons = document.querySelectorAll('.filterClose');
+
+        burgerIconFilters.forEach(function (burgerIconFilter, index) {
+            burgerIconFilter.addEventListener("click", function () {
+                toggleMenu(index);
+            });
+
+            closeButtons[index]?.addEventListener("click", function () {
+                toggleMenu(index);
+            });
+        });
+
+        function toggleMenu(index) {
+            burgerIconFilters[index].classList.toggle("active");
+            burgerFilters[index].classList.toggle("active");
+
+            const isOpen = burgerFilters[index].classList.contains("active");
+            document.body.style.overflow = isOpen ? "hidden" : "";
+        }
+    };
+    initBurgerFilterCards();
+
+    //initPagination
+    const initPagination = () => {
+        const pagination = document.getElementById('pagination');
+        const slidesContainer = document.querySelector('.cardsBlock_cards');
+
+        const itemsPerPage = 9;
+        const totalItems = slidesContainer.children.length;
+
+        if (totalItems > itemsPerPage) {
+            const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+            for (let i = 1; i <= totalPages; i++) {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.href = '#';
+                a.textContent = i;
+                li.appendChild(a);
+                pagination.appendChild(li);
+            }
+
+            pagination.addEventListener('click', function (event) {
+                if (event.target.tagName === 'A') {
+                    const pageNumber = parseInt(event.target.textContent);
+                    showPage(pageNumber);
+
+                    // Удаляем класс актив со всех элементов
+                    const paginationItems = pagination.querySelectorAll('a');
+                    paginationItems.forEach(item => item.classList.remove('active'));
+
+                    // Добавляем класс актив текущему элементу
+                    event.target.classList.add('active');
+                }
+            });
+
+            function showPage(pageNumber) {
+                const start = (pageNumber - 1) * itemsPerPage;
+                const end = start + itemsPerPage;
+
+                for (let i = 0; i < totalItems; i++) {
+                    if (i >= start && i < end) {
+                        slidesContainer.children[i].style.display = 'block';
+                    } else {
+                        slidesContainer.children[i].style.display = 'none';
+                    }
+                }
+
+                // Скрываем/показываем элементы пагинации в зависимости от текущей страницы
+                const paginationItems = pagination.querySelectorAll('li');
+                paginationItems.forEach((item, index) => {
+                    if (index < totalPages) {
+                        item.style.display = 'block';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                });
+            }
+
+            showPage(1);
+        } else {
+            pagination.style.display = 'none';
+
+            // Показываем все элементы, так как их меньше, чем itemsPerPage
+            for (let i = 0; i < totalItems; i++) {
+                slidesContainer.children[i].style.display = 'block';
+            }
+        }
+    };
+
+    initPagination();
+
+
 });
